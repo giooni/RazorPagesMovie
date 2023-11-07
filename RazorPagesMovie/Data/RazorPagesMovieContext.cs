7baaf27f7a9e351ba67a46d5210dc80db514a57a
giorgi <ogiorgi69@gmail.com>
2023-11-07 13:24:27 +0400

    Add project files.

---
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Movie;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Movie.Movie> Movie { get; set; } = default!;
+    }
+}
